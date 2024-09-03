36edb7c0441df7028072ae323e946c80f7ca293f
Anna Burnett <annairishburnett@gmail.com>
2024-09-03 19:20:01 -0400

    added community solutions

--- 8-kyu/find-max-and-min-values-of-list.js
+++ 8-kyu/find-max-and-min-values-of-list.js
@@ -16,9 +16,12 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P:
-//R:
-//E:
+//P: make two functions that receive a list of integers as input
+//R: return the largest and lowest number in that list (each function returns one number)
+//E: [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
 //P: Psuedo code -> see comments in solution
 
 
@@ -31,7 +34,31 @@
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
 
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
 
 
+///
+
+var min = function(list){
+    list.sort((a, b) => (a - b));
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => (b - a));
+    return list[0];
+}
 
 
+////
+
+
+var min = function(list){
+    return Math.min(...list)
+}
+
+var max = function(list){
+    
+    return Math.max(...list)
+}
