93775d0f771d6926ebf304aff234137fe4b93538
Anna Burnett <annairishburnett@gmail.com>
2024-09-09 16:10:54 -0400

    added solutions

---
+++ 8-kyu/convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,33 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P:
+//R:
+//E:
+//P: Psuedo code -> see comments in solution
+
+
+
+
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+
+
+
+
