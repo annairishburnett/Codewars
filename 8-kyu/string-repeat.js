a972dcc9d76cb805949b82f4d749d81445deffd1
Anna Burnett <annairishburnett@gmail.com>
2024-09-18 18:08:04 -0400

    added description of kata

---
+++ 8-kyu/string-repeat.js
@@ -0,0 +1,33 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P:
+//R:
+//E:
+//P: Psuedo code -> see comments in solution
+
+
+
+
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+
+
+
+
