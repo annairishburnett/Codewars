0064de5b470cb83ceb69e9ba1fd2c72e9d5fcb99
Anna Burnett <annairishburnett@gmail.com>
2024-09-23 17:19:55 -0400

    added my solution and top solutions

--- 8-kyu/returning-strings.js
+++ 8-kyu/returning-strings.js
@@ -1,20 +1,24 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
 
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P:
-//R:
-//E:
+//P: Create a function that takes in a name
+//R: "Hello, <name> how are you doing today?"
+//E: Anna --> "Hello, Anna how are you doing today?"
 //P: Psuedo code -> see comments in solution
 
 
 
+//take in a name, return sentence with that name inserted into it:
+const greet = name => `Hello, ${name} how are you doing today?`;
 
 
 
@@ -22,7 +26,16 @@
 
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+}
 
+const greet = name => `Hello, ${name} how are you doing today?`; 
+
+// Return a greeting string
+function greet(name) {
+    return "Hello, " + name + " how are you doing today?";
+}
 
 
 
