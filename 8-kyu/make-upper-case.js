1ad15458dd7e3c3c4872d93794fecbbd0be78a04
Anna Burnett <annairishburnett@gmail.com>
2024-09-26 11:09:31 -0400

    solutions and pseudo code added

--- 8-kyu/make-upper-case.js
+++ 8-kyu/make-upper-case.js
@@ -2,26 +2,33 @@
 
 
 
-
+// Write a function which converts the input string to uppercase.
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P:
-//R:
-//E:
+//P: Take a string and convert it to uppercase letters
+//R: an uppercase string
+//E: 'hello' --> 'HELLO'
 //P: Psuedo code -> see comments in solution
 
 
 
-
-
+//create a function that takes in a str as a parameter
+function makeUpperCase(str){
+    return str.toUpperCase();//convert the str to upper case and return it
+}
 
 
 
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
+const makeUpperCase = str => str.toUpperCase();
+
+function makeUpperCase(str) {
+    return str.toLocaleUpperCase();
+}
 
 
 
