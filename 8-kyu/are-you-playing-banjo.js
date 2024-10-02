11061b7908f08d93d7119ae504d0758e266cd0f3
Anna Burnett <annairishburnett@gmail.com>
2024-10-02 12:22:35 -0400

    added pseudo code

--- 8-kyu/are-you-playing-banjo.js
+++ 8-kyu/are-you-playing-banjo.js
@@ -1,8 +1,14 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
 
+// The function takes a name as its only argument, and returns one of the following strings:
 
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
 
 
 
@@ -10,20 +16,57 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
+//P: Create a function which answers the question "Are you playing banjo?". The function takes a name as its only argument. Names given are always valid strings.
+//R: one of the following strings:
+    // name + " plays banjo" 
+    // name + " does not play banjo"
+//E: "Rudolfo" --> "Rudolfo plays bajo"
 //P: Psuedo code -> see comments in solution
 
 
+//First iteration
+//create a function that takes in a name as a single parameter, split it into an array, if the first element of the array in it's lowercase form is equal to r, return "Name plays banjo", otherwise "Name does not play banjo"
+function areYouPlayingBanjo(name) {
+    const array = name.split('');
+    if(array[0].toLowerCase() === 'r'){
+       return `${name} plays banjo`
+    }
+    else return `${name} does not play banjo`
+}
 
-
+//See solutions below for drier code
 
 
 
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
 
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
 
+function areYouPlayingBanjo(name) {
+    if (name[0].toLowerCase() === 'r') {
+      return name + ' plays banjo';
+    } else {
+      return name + ' does not play banjo';
+    }
+}
+
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if (name[0] == 'R' || name[0] == 'r')
+      return name + " plays banjo";
+    else
+      return name + " does not play banjo";
+}
+
+function areYouPlayingBanjo(name) {
+    return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
+}
+
+function areYouPlayingBanjo(name) {
+    return name[0].toLowerCase() == "r" ? name + " plays banjo" : name + " does not play banjo";
+}
 
 
