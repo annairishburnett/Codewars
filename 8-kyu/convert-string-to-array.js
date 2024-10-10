88df2f1e025ea9e16df1cebb5ec96d35117f36cc
Anna Burnett <annairishburnett@gmail.com>
2024-10-10 12:11:46 -0400

    added PREP and pseudo code

--- 8-kyu/convert-string-to-array.js
+++ 8-kyu/convert-string-to-array.js
@@ -2,7 +2,11 @@
 
 
 
+// Write a function to split a string and convert it into an array of words.
 
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 
 
 
@@ -10,18 +14,27 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
+//P: Write a function to split a string and convert it into an array of words
+//R: an array of strings
+//E: "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 //P: Psuedo code -> see comments in solution
 
 
-
-
+//create a function that takes in a single parameter (a string) 
+function stringToArray(string){
+    return string.split(' ');//split the string at the spaces and return the resulting array
+}
 
 
 
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+const stringToArray = string => string.split(' ');
+
+function stringToArray(string){
+    return string.split(' ');
+}
 
 
 
