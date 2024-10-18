305d51dd8a300a9a5984cbf44ae336af237d75d0
Anna Burnett <annairishburnett@gmail.com>
2024-10-18 18:59:53 -0400

    added PREP and pseudo code

--- 8-kyu/remove-exclamation-marks.js
+++ 8-kyu/remove-exclamation-marks.js
@@ -2,7 +2,7 @@
 
 
 
-
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 
 
@@ -10,20 +10,33 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
+//P: Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+//R: a string with no exclamation marks
+//E: 'Hello!' --> 'Hello'
 //P: Psuedo code -> see comments in solution
 
 
-
-
+//create a function that takes in a string
+//remove all exclamation marks and return a string
+const removeExclamationMarks = s => s.replace(/!/g,"") ;
 
 
 
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi, '');
+}
 
+function removeExclamationMarks(s) {
+    return s.split('!').join('');
+}
+
+function removeExclamationMarks(s) {
+    return s.replaceAll('!', '');
+}
+
+const removeExclamationMarks = s => s.replace(/!/g,"") ;
 
 
 
