8508310f5e60f11bc711d36b7e29a66035ff7ca1
Anna Burnett <annairishburnett@gmail.com>
2024-11-05 16:32:05 -0500

    updated to correct date

---
+++ 8-kyu/all-star-coding-challenge-number-18.js
@@ -0,0 +1,43 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+
+
+
+
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Edge Cases, Pseudo Code:
+
+//P: 
+//R: 
+//E: 
+//E:
+//P: Psuedo code -> see comments in solution
+
+
+
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
