866f3f0d5faf68c6579bf2990e3737a4add5337c
Anna Burnett <annairishburnett@gmail.com>
2024-11-13 11:49:17 -0500

    updated date

--- 8-kyu/short-long-short.js
+++ 8-kyu/short-long-short.js
@@ -1,7 +1,15 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
+
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+// ShortLongShort.solution("1", "22"); // returns "1221"
+// ShortLongShort.solution("22", "1"); // returns "1221"
 
 
 
@@ -10,10 +18,11 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+//R: a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside
+//E: ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+//E: the string may be empty ( zero length )
 //P: Psuedo code -> see comments in solution
 
 
