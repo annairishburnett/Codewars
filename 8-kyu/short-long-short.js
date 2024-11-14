16733fca0ff9e0c5047ba0d0a0c4c1e213b12b1b
Anna Burnett <annairishburnett@gmail.com>
2024-11-14 11:56:18 -0500

    added PREP and comments

--- 8-kyu/short-long-short.js
+++ 8-kyu/short-long-short.js
@@ -26,22 +26,59 @@
 //P: Psuedo code -> see comments in solution
 
 
+//1st Iteration
+//create a function that takes in two parameters
+function solution(a, b){
+    if(a.length > b.length) return `${b}${a}${b}`//if a is longer than b, return a sandwiched by b
+    else if(a.length < b.length) return `${a}${b}${a}`//else if a is shorter than b, return b sandwiched by a
+}//should refactor with a ternary operator:
 
+
+//2nd Iteration
+function solution(a, b){
+    return a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`;
+}
 
 
 
 
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
+function solution(a, b) {
+    return a.length < b.length ? a + b + a : b + a + b
+}
 
 
 
+// Using if...else, explicit block syntax
+function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
+}
+
+// Using if..., implicit else, compressed syntax
+function solution(a, b) {
+    if (a.length < b.length) return a + b + a;
+    return b + a + b;
+}
+
+// Using ternary conditional operator
+function solution(a, b) {
+    return (a.length < b.length) ? (a + b + a) : (b + a + b);
+}
+
+// ES6 arrow function
+var solution = (a, b) =>
+    (a.length < b.length) ? (a + b + a) : (b + a + b);
 
 
 
-
-
-
+const solution = (a, b) => {
+    return a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
+}
 
 
 
