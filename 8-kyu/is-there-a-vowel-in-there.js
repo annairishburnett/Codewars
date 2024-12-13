0420ac4960dd8aff2888ebc0fb66bc25317170aa
Anna Burnett <annairishburnett@gmail.com>
2024-12-13 08:33:57 -0500

    added description and PREP

--- 8-kyu/is-there-a-vowel-in-there.js
+++ 8-kyu/is-there-a-vowel-in-there.js
@@ -1,16 +1,20 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
 
+// If they are, change the array value to a string of that vowel.
+
+// input [100,100,116,105,117,121] => [100,100,116,"i","u",121] output Return the resulting array.
 
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: Input will be an array of numbers.
+//R: If any of the numbers are the character codes for lower case vowels (a, e, i, o, u), return the array with those numbers converted to the corresponding string of that vowel.
+//E: [100,100,116,105,117,121] => [100,100,116,"i","u",121]
+//E: No edge cases are listed. Will there be any empty arrays? The assumption is no. 
 //P: see steps below
 
 
