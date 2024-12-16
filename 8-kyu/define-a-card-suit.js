c4425ece25dfc95fab0b537bd42f2141f5e7593f
Anna Burnett <annairishburnett@gmail.com>
2024-12-16 08:19:52 -0500

    added description and PREP

--- 8-kyu/define-a-card-suit.js
+++ 8-kyu/define-a-card-suit.js
@@ -1,16 +1,26 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
 
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
 
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: Input is a number with a card suit. Will there be any inputs without card suits? No. Will there be numbers with more than one digit? Yes.
+//R: Return the name of the card suit in the form of a string.
+//E: ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+//E: None provided, so we will assume none until we submit the solution and see if we get any errors.
 //P: see steps below
 
 
