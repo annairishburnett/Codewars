b06d69995fde56f00f6f09232feb54ce3e394ddf
Anna Burnett <annairishburnett@gmail.com>
2025-01-16 15:50:53 -0500

    added new codewars challenge

---
+++ 8-kyu/multiply.js
@@ -0,0 +1,45 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+    a * b
+}
+  
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P: The code does not execute properly.
+//R: Not given - we can assume it's a number that's the product of a and b
+//E: 2 * 2 = 4
+//P: Psuedo code -> see comments in solution
+
+
+
+function multiply(a, b){
+    return a * b //The function was missing the return
+}
+  
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+multiply = function (a, b) {
+    return a * b;
+}
+
+
+multiply = (a, b) => a * b;
+
+
+
+//EXPLANATION FOR CODE ABOVE:
+
+//Created a variable whose value is the function and added the return
+
+//Created an array function with an implicit return
