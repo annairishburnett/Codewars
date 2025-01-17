fc4bb6d13fd3f82c0b55191964263235383fea3a
Anna Burnett <annairishburnett@gmail.com>
2025-01-17 14:53:42 -0500

    added new codewars challenge

---
+++ 8-kyu/convert-number-to-string.js
@@ -0,0 +1,38 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P:
+//R:
+//E: 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+//P: Psuedo code -> see comments in solution
+
+
+
+
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+
+
+
+
