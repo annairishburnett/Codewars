d418b045b371af819e89188979461adda026f6e8
Anna Burnett <annairishburnett@gmail.com>
2025-01-17 14:47:09 -0500

    added codewars challenge function-1-hello-world.js

---
+++ 8-kyu/function-1-hello-world.js
@@ -0,0 +1,32 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P:
+//R:
+//E:
+//P: Psuedo code -> see comments in solution
+
+
+
+
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+
+
+
+
