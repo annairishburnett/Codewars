782de308178f076d31fb4a5e57519d6bd7ee4596
Anna Burnett <annairishburnett@gmail.com>
2025-01-17 15:06:48 -0500

    added pseudo code and solution

---
+++ 8-kyu/you-cant-code-under-pressure-1.js
@@ -0,0 +1,43 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P: an integer
+//R: return an integer
+//E: 2 -> 4
+// 5 --> 10
+//P: Psuedo code -> see comments in solution
+
+
+//create a function with one parameter, i for integer
+function doubleInteger(i) {
+    //multiple input by 2 and return it
+    return i * 2;
+}
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+const doubleInteger = i => 2 * i;
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+}
+
+
+
+
