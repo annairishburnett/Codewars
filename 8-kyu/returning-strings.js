bd11e208853b2bb359a3fe4b2cd68202dd07f55a
Anna Burnett <annairishburnett@gmail.com>
2025-01-19 16:15:39 -0500

    initial commit

---
+++ 8-kyu/returning-strings.js
@@ -0,0 +1,29 @@
+//CODEWARS CODING CHALLENGE DESCRIPTION:
+
+
+
+
+
+
+//MY SOLUTION:
+//Parameters, Return, Examples, Pseudo Code:
+
+//P:
+//R:
+//E:
+//P: Psuedo code -> see comments in solution
+
+
+
+
+
+
+
+
+//OTHER TOP SOLUTIONS FROM CODEWARS USERS:
+
+
+
+
+
+
