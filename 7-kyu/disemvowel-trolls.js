a6f452fbed96170df74999b83c4cf64c98b64021
Anna Burnett <annairishburnett@gmail.com>
2025-01-21 10:55:01 -0500

    added solutions and PREP to codewars challenge

--- 7-kyu/disemvowel-trolls.js
+++ 7-kyu/disemvowel-trolls.js
@@ -1,7 +1,15 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// Trolls are attacking your comment section!
 
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
 
 
 
@@ -10,13 +18,18 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
+//P: Your task is to write a function that takes a string and return a new string with all vowels removed.
+//R: a string with no vowels
+//E: "This website is for losers LOL!" --> "Ths wbst s fr lsrs LL!"
 //P: Psuedo code -> see comments in solution
 
 
+let string = "This website is so funny LOL!";
 
+//create a function that removes uppercase and lowercase vowels and returns the new string
+function disemvowel(str){
+    return str.replace(/[aeiouAEIOU]/gi, '');
+}
 
 
 
@@ -24,6 +37,20 @@
 //OTHER TOP SOLUTIONS FROM CODEWARS USERS:
 
 
+disemvowel = str => str.replace(/[aeiou]/gi,'');
 
 
 
+const vowels = 'aeiou';
+
+function disemvowel(str) {
+  return str
+    .split('')
+    .filter(letter => !vowels.includes(letter.toLowerCase()))
+    .join('');
+}
+
+
+function disemvowel(str) {
+    return (str || "").replace(/[aeiou]/gi, "");
+}
