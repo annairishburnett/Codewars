85363092a19e8c1d1d60222997dd5a2239dc3bf4
Anna Burnett <annairishburnett@gmail.com>
2025-01-28 15:20:12 -0500

    added description and PREP

--- 7-kyu/complementary-dna.js
+++ 7-kyu/complementary-dna.js
@@ -1,6 +1,17 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
 
 
 
@@ -10,12 +21,15 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all.
+//R: complementary dna to input in the form of a string
+//E: "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+//E: none given
 //P: Psuedo code -> see comments in solution
 
+
+//1st Iteration
 
 
 
