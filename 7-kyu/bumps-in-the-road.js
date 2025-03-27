050856f92f71abaf6e01170a4f68236dffc57d5a
Anna Burnett <annairishburnett@gmail.com>
2025-03-27 11:43:01 -0400

    added my solution interation process + comments

--- 7-kyu/bumps-in-the-road.js
+++ 7-kyu/bumps-in-the-road.js
@@ -22,13 +22,62 @@
 
 // 1 - create a function that takes in a string
 // 2 - remove all spaces from the string using .replace(/ /g, ''), then check number of characters with .length
+        //skip this step, do .split(" ") instead
+        //except we don't know if there are spaces or not??? try out a few things
+        //also don't know if the "n"'s will all be lowercase???
 // 3 - but we also have to check if the character is "n" or "_", so we can use if else statement with === comparison
 // 4 - but to do that we need to isolate one of the characters out of the string, so we need to convert it into an array, then select one of the elements with array[0] - should work since the strings will either contain "n" OR "_"
 // 5 - if the string is "_", return "Woohoo!", if the string is "n" AND length of the string is 15 or more, return "Car Dead"
 //call the function
 
 
+//1st ITERATION
 
+const string = "n n n n n n n n n n n n n n n n n n n n";
+
+function bump(x){
+    const arr = x.split(' ');
+    const char = arr[0];
+
+    if((char === 'n') && (arr.length >= 15)){
+        return "Car Dead"
+    }else{
+        return "Whoohoo!"
+    }
+}
+
+console.log(bump(string));
+
+
+//It worked in VSCode! Now to check if the string input in Codewars is the same as what I imagined for the first iteration.
+    //It did not work in Codewars, here's the error message: expected 'Whoohoo!' to equal 'Woohoo!'
+    //Test case strings were formatted like so: "__nn_nnnn__n_n___n____nn__nnn"
+        //So I have to redo the function to accomodate that type of input:
+        // 1 - use .split('')
+        // 2 - use .filter(char => char === "n") to remove all of the "_" characters
+        // 3 - then use .length on the array to get the number of "n" characters
+        // 4 - don't need to create the const char or the char === "n" if else validation anymore because we're removing the "_" characters
+        // 5 - use a ternary operator to solve in one line with a return at the beginning
+        
+//2nd ITERATION
+
+function bump(x){
+    return x.split('').filter(char => char === 'n').length >= 15 ? "Car Dead" : "Woohoo!";
+}
+
+console.log(bump(string));
+//It worked in VSCode! Now to check if it works in Codewars.
+    //It did not work in Codewars, here's the error message: expected 'Car Dead' to equal 'Woohoo!'
+    //Test case strings it failed was: "nnn_n__n_n___nnnnn___n__nnn__"
+        //Analysis of that string: there are 15 "n"'s, so according to the kata description it should be "Car Dead" -> check the kata description again to be sure you read it correctly
+            // I did not -> it says if you encounter 15 or less bumps, you arrive home safely
+                //It's an easy fix, switch .length >= 15 TO .length > 15
+
+//3RD ITERATION
+function bump(x){
+    return x.split('').filter(char => char === 'n').length > 15 ? "Car Dead" : "Woohoo!";
+}
+//It worked in Codewars! Success!
 
 
 
