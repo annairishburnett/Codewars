f4cdbb69ea567452455fdc8a1be5f79a97b93a70
Anna Burnett <annairishburnett@gmail.com>
2025-03-28 09:37:28 -0400

    added pseudo code

--- 7-kyu/filter-the-number.js
+++ 7-kyu/filter-the-number.js
@@ -1,7 +1,14 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
 
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
 
 
 
@@ -10,12 +17,16 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: A string of numbers and letters mixed up. Any empty strings? Any strings with no numbers in them? Any other funny business? No
+//R: return all the numbers in that string in the order they occur
+//E: "He11o th3r3 y0u!" -> 11330
+//E: No edge cases were given.
 //P: Psuedo code -> see comments in solution
 
+// 1 - create a function that takes in a string
+// 2 - use .match(/[0123456789]/g) to find all of the numbers 
+        //not sure if that will work...
+// 3 - return the result and call the function
 
 
 
@@ -23,12 +34,6 @@
 
 
 //TOP SOLUTIONS FROM CODEWARS USERS:
-
-
-
-
-
-
 
 
 
