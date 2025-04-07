96061d13f4d1199af1abe47e34b31779eac77a7c
Anna Burnett <annairishburnett@gmail.com>
2025-04-07 11:14:59 -0400

    added description and pseudo code

--- 7-kyu/odd-even-string-sort.js
+++ 7-kyu/odd-even-string-sort.js
@@ -1,7 +1,18 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// Given a string s, your task is to return another string such that even-indexed and odd-indexed characters of s are grouped and the groups are space-separated. Even-indexed group comes as first, followed by a space, and then by the odd-indexed part.
 
+// Examples
+// input:    "CodeWars" => "CdWr oeas"
+//            ||||||||      |||| ||||
+// indices:   01234567      0246 1357
+// Even indices 0, 2, 4, 6, so we have "CdWr" as the first group.
+// Odd indices are 1, 3, 5, 7, so the second group is "oeas".
+// And the final string to return is "Cdwr oeas".
+
+// Notes
+// Tested strings are at least 8 characters long.
 
 
 
@@ -10,13 +21,20 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: a string s that will contain at least 8 characters, all letters, no nums or symbols or funny business
+//R: return another string such that even-indexed and odd-indexed characters of s are grouped and the groups are space-separated. Even-indexed group comes as first, followed by a space, and then by the odd-indexed part.
+//E: input:    "CodeWars" => "CdWr oeas"
+//            ||||||||      |||| ||||
+// indices:   01234567      0246 1357
+//E: no edge cases given
 //P: Psuedo code -> see comments in solution
 
+// 1 - create a function that takes in a string
+// 2 - create two conts, even and odd, and set them equal to s.reduce(), the first reducing for only even characters and the second for only odd characters
+// 3 - return with tick marks ${even} with a space then ${odd}, which will give us a string of the sorted characters with a space in the middle
+// 4 - call the function
 
+//1ST ITERATION
 
 
 
