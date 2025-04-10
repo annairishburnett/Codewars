e530eb7774b869ce8e63e4d95792ffc6d786f96b
Anna Burnett <annairishburnett@gmail.com>
2025-04-10 08:58:52 -0400

    added my iterative solution process with comments

--- 7-kyu/switcheroo.js
+++ 7-kyu/switcheroo.js
@@ -31,6 +31,45 @@
 // 5 - .split('') to create an array
 // 6 - .map(char => char === 'a' ? char.replace('b') : char.replace('a')) -> this should leave c alone
 // 7 - use .join('') to turn it back into a string
+// 8 - return the value and call the function
+
+
+//1ST ITERATION
+let string = 'aabacbaa';
+
+function switcheroo(str){
+        return str.split('').map(char => char === "a" ? char.replace("b") : char.replace("a")).join('');
+}
+
+console.log(switcheroo(string)); //should return "bbabcabb"
+//return was "aabacbaa"
+//.replace is for strings, so maybe we don't need char.replace(""), we can just used the letter "a"
+
+
+//2ND ITERATION
+function switcheroo(str){
+        return str.split('').map(char => char === "a" ? "b" : "a").join('');
+}
+//returned: "bbabaabb", should be: "bbabcabb"
+//because if it's not a, it replaces it with a and c is not a
+//add an || char === "c" with another ternary operator or can we just use ||???
+
+
+//3RD ITERATION
+function switcheroo(str){
+        return str.split('').map(char => char === "c" || (char === "a" ? "b" : "a")).join('');
+}
+//returned: "bbabtrueabb", should be: "bbabcabb"
+//getting closer! :-) use ternary operator, not ||
+
+//4TH ITERATION
+function switcheroo(str){
+        return str.split('').map(char => char === "c" ? "c" : (char === "a" ? "b" : "a")).join('');
+}
+//returned: "bbabcabb", should be: "bbabcabb"
+//Success on VSCode! Now time to try it on Codewars
+
+
 
 
 
