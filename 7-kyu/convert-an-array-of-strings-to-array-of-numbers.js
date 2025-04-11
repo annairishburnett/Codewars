39be0ae9cb81fe0d240f6ec9d3857f35d9134ef7
Anna Burnett <annairishburnett@gmail.com>
2025-04-11 09:28:46 -0400

    added codewars community answers and comments

--- 7-kyu/convert-an-array-of-strings-to-array-of-numbers.js
+++ 7-kyu/convert-an-array-of-strings-to-array-of-numbers.js
@@ -47,13 +47,25 @@ console.log(toNumberArray(array)); // [1.2, 2.3, 3.4]
 //TOP SOLUTIONS FROM CODEWARS USERS:
 
 
+//I don't know what parseFloat does -> look it up on MDN
+    //The parseFloat() function parses a string argument and returns a floating point number.
+    //Return value: A floating point number parsed from the given string, or NaN when the first non-whitespace character cannot be converted to a number.
+    //Note: JavaScript does not have the distinction of "floating point numbers" and "integers" on the language level. parseInt() and parseFloat() only differ in their parsing behavior, but not necessarily their return values. For example, parseInt("42") and parseFloat("42") would return the same value: a Number 42.
+function toNumberArray(stringarray){
+  return stringarray.map(parseFloat);
+}
+
+//OR 
+const toNumberArray = arr => arr.map(parseFloat);
+
+
+//Basically the same as my answer
+const toNumberArray = stringarray => stringarray.map(Number);
 
 
 
-
-
-
-
+//Used .map to loop through the array and the unary plus (+) operator to convert each string to a number
+toNumberArray = a => a.map(e => +e)
 
 
 
