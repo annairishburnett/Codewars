df15d24d4ea2e986c133f08d411c7d45cd655a6b
Anna Burnett <annairishburnett@gmail.com>
2025-04-11 09:17:21 -0400

    added description and pseudo code

--- 7-kyu/convert-an-array-of-strings-to-array-of-numbers.js
+++ 7-kyu/convert-an-array-of-strings-to-array-of-numbers.js
@@ -1,8 +1,15 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Oh no!
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
 
+// You need to cast the whole array to the correct type.
 
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
 
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
 
 
 
@@ -10,10 +17,10 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: input is an array of numbers represented as strings. Will there be any letters, special characters, empty arrays, or funny business? Yes - floated values (numbers with decimals)
+//R: an array of numbers
+//E: ["1", "2", "3"] to [1, 2, 3]
+//E: Note that you can receive floats as well
 //P: Psuedo code -> see comments in solution
 
 
