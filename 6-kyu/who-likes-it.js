be66d9ecbfd9f053d7440b8f065581c189d2ab35
Anna Burnett <annairishburnett@gmail.com>
2025-04-14 10:28:08 -0400

    added solution iterations with comments

--- 6-kyu/who-likes-it.js
+++ 6-kyu/who-likes-it.js
@@ -34,6 +34,96 @@
 // 4 - so, we'd do array.reduce((sentence,name)), then within that we'd use if/else statements and comparison with array.length to get the correct sentence formation
 // 5 - for example, if(array.length === 1){sentence += `${name} likes this`}
 // 6 - return the value and call the function
+        //using .reduce in this way returns something invisible, so go back to original plan
+        //also got an invisible return when I used let sentence = "" and a forEach loop
+        //checked with ChatGPT, a loop doesn't run if the array is empty, so move the first option arr.length === 0 outside of the loop, THEN do an else conditional with the arr.forEach
+        //So the problem with .reduce wasn't the reduce, it was running .reduce on an empty array
+        //Try it again with the arr.length === 0 outside of the arr.reduce
+        //but it doesn't really make sense to use reduce to eliminate creating a variable when you need to create the variable for the arr.length === 0 option anyway, so use forEach
+        //actually, we don't need the loop, we can just use if/else conditionals and the array elements with their indexes since we only have to go up to the 3rd element/name and then use math from there to get the "and # others like this"
+        
+
+
+//1ST ITERATION
+let array = [];
+
+function whoLikesThis1(arr) {
+     return arr.reduce((sentence, name) => {
+        if(arr.length === 0){
+                sentence += "no one likes this";
+                return sentence;
+        }
+     }, "");
+}
+console.log(whoLikesThis1(array));// returned something invisible
+
+
+
+//2ND ITERATION
+let array1 = [];
+
+function whoLikesThis2(arr) {
+     let sentence = "";
+     
+     arr.forEach((name, index) => {
+         if(arr.index === 0){
+                sentence += "no one likes this"
+         }
+     });
+     
+     return sentence;
+}
+
+console.log(whoLikesThis2(array1));// returned something invisible
+
+
+
+//3rd ITERATION
+let array2 = [];
+
+function whoLikesThis3(arr) {
+     let sentence = "";
+     
+     if(arr.length === 0) {
+         sentence = "no one likes this";
+     }else{
+         arr.forEach((name, index) => {
+             // Build the sentence based on # of names
+             sentence += name + " likes this"; 
+         });
+     }
+     
+     return sentence;
+}
+
+console.log(whoLikesThis3(array2));// "no one likes this"
+
+
+//4TH ITERATION
+let array3 = ["Evi", "Lea", "Anna", "Laura"];
+
+function likes(names) {
+     let sentence = "";
+     
+     if(names.length === 0) {
+        sentence = "no one likes this";
+     }else if(names.length === 1){
+        sentence = `${names[0]} likes this`;
+     }else if(names.length === 2){
+        sentence = `${names[0]} and ${names[1]} like this`;
+     }else if(names.length === 3){
+        sentence = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+     }else{
+        sentence = `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+     }
+     
+     return sentence;
+}
+
+console.log(likes(array3));// 
+//It works on VSCode! Try it out on Codewars
+//It works! I think I panicked and checked what was wrong on ChatGPT because I thought a 6-kyu kata would be much harder/more complex, but the answer doesn't include anything new, or anything that tricky
+//Stay calm and think through the simplest ways of doing things and you'll be fine :) 
 
 
 
