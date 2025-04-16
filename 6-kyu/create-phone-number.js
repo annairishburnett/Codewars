b88a9b2c5cdc33406ba7950f4f1498c184115e3f
Anna Burnett <annairishburnett@gmail.com>
2025-04-16 11:55:59 -0400

    added description and pseudo code

--- 6-kyu/create-phone-number.js
+++ 6-kyu/create-phone-number.js
@@ -1,21 +1,27 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
 
-
-
+// Don't forget the space after the closing parentheses!
 
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: Create a function that takes in an array of 10 integers (between 0 and 9). No letters, special characters, or funny business? No
+//R: Returns a string of those numbers in the form of a phone number. The returned format must be correct in order to complete this challenge. Don't forget the space after the closing parentheses!
+//E: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] => returns "(123) 456-7890"
+//E: None given.
 //P: Psuedo code -> see comments in solution
 
+// 1 - create a function that takes in an array
+// 2 - simple and not very elegant, but you could use a template string and the array indices 
+// 3 - return the value and call the function
 
 
 
