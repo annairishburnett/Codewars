61f26e762573339ad9d15e640b85c784aeb655f6
Anna Burnett <annairishburnett@gmail.com>
2025-04-21 10:09:50 -0400

    added description and pseudo code

--- 6-kyu/counting-duplicates.js
+++ 6-kyu/counting-duplicates.js
@@ -1,6 +1,15 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
 
 
 
@@ -10,10 +19,16 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: Input will be a string that contains only alphabetic characters (both uppercase and lowercase) and numeric digits. Any empty strings or other types of inputs? No
+//R: return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string
+//E: "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+//E: None given
 //P: Psuedo code -> see comments in solution
 
 
