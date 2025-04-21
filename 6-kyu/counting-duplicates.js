96fe527321bf6809f149b239f1c4a73ba2e4f5e8
Anna Burnett <annairishburnett@gmail.com>
2025-04-21 10:10:35 -0400

    added description and pseudo code

--- 6-kyu/counting-duplicates.js
+++ 6-kyu/counting-duplicates.js
@@ -32,6 +32,19 @@
 //P: Psuedo code -> see comments in solution
 
 
+// 1 - create a function that takes in a string
+// 2 - convert the string to all lowercase characters with .toLowerCase()
+        //Actually maybe don't do this because they want to know about lowercase and uppercase letters (see 3rd example)
+// 3 - you could use new Set() to get the list of unique characters, then create variables for each and set them equal to 0, then loop through original string (converted to an array) and add each occurence to the total count for that character
+// 4 - then use template literal to create sentence structure needed for the return
+// 5 - could also do all of this inside of .reduce and set up the template literal as the starting value for the accumulator
+        //Actually that might be difficult since we don't know what the structure will be until we break down the string
+// 6 - will need to use if(...) return statements and template literals depending on the conditions
+// 7 - return the value and call the function
+// 8 - this one seems tricky - I feel like I'm missing something, maybe regex logic? We'll see! Just try your best!
+
+
+//1ST ITERATION
 
 
 
