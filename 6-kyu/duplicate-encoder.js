58968a3af17b3bc2ff5b09d97ec67fc1c02b615e
Anna Burnett <annairishburnett@gmail.com>
2025-04-23 09:33:15 -0400

    added description and pseudo code

--- 6-kyu/duplicate-encoder.js
+++ 6-kyu/duplicate-encoder.js
@@ -1,8 +1,16 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
 
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
 
 
@@ -10,13 +18,19 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: input is a string, can contain spaces and special characters
+//R: a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+//E: "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+//E: Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 //P: Psuedo code -> see comments in solution
 
-
+// 1 - create a function that takes in a string
+// 2 - use .replace() with a regex 1+ to check for more than one occurence, and a ternary operator to determine "(" OR ")"
+// 3 - return the value and call the function
+// 4 - not well-thought-out answer, but will need to do some trial and error to get a better idea of what to do
 
 
 
