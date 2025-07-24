9ac63839a0fba725b2231865d906892790735913
Anna Burnett <annairishburnett@gmail.com>
2025-07-24 12:24:23 -0400

    added first two solution iterations, need to fix error still

--- 8-kyu/exclamation-mark-series-num-one-remove-an-exclamation-mark.js
+++ 8-kyu/exclamation-mark-series-num-one-remove-an-exclamation-mark.js
@@ -16,20 +16,32 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: input is always a string
-//R: return the string without an exclamation mark at the end
-//E: "Hi!"     ---> "Hi"
-// "Hi!!!"   ---> "Hi!!"
-// "!Hi"     ---> "!Hi"
-// "!Hi!"    ---> "!Hi"
-// "Hi! Hi!" ---> "Hi! Hi"
-// "Hi"      ---> "Hi"
-//E: no edge cases or funny business
-//P: Psuedo code -> see comments in solution
-
 //1 - create a function that takes in a string
-//2 - use .removeAll() and select '!'
-//3 - return the string and call the function
+//2 - use .replaceAll() and select '!', or could use .replace() with the g global flag to select all and replace with an empty string ''
+//3 - return the string and call the function, console.log() result to check function is working
+
+
+let string = "!Hi!!!";
+
+function removeExclamationMarks(str){
+    return str.replace(/!/g, '');
+}
+
+console.log(removeExclamationMarks(string));
+//Didn't work with .replace(/'!'/g, ''), but worked with .replaceAll('!', '')
+//Ohh, right, I don't need the quotations around the ! exclamation mark with the regex
+//Now it works :-) 
+
+
+//2nd Iteration with codewars function name and params
+function remove(string){
+    return string.replace(/!/g, '');
+}
+//Oops, misread description and examples - remember not to rush and assume I know what they want me to do without fully processing the directions :-)
+//Directions: Remove an exclamation mark from the end of a string
+
+//3rd Iteration
+
 
 
 
