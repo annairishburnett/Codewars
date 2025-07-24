eef0a8fd402e565e41a099a69d2d0eb80efa9d73
Anna Burnett <annairishburnett@gmail.com>
2025-07-24 12:06:22 -0400

    added description and pseudo code

--- 8-kyu/exclamation-mark-series-num-one-remove-an-exclamation-mark.js
+++ 8-kyu/exclamation-mark-series-num-one-remove-an-exclamation-mark.js
@@ -1,8 +1,14 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
 
-
-
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
 
 
 
@@ -10,14 +16,20 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: input is always a string
+//R: return the string without an exclamation mark at the end
+//E: "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+//E: no edge cases or funny business
 //P: Psuedo code -> see comments in solution
 
-
-
+//1 - create a function that takes in a string
+//2 - use .removeAll() and select '!'
+//3 - return the string and call the function
 
 
 
