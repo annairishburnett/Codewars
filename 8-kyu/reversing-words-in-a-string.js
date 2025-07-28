be5a432b72dca0b760ee809f50f9b651fa91ef2f
Anna Burnett <annairishburnett@gmail.com>
2025-07-28 16:04:39 -0400

    added description and PREP

--- 8-kyu/reversing-words-in-a-string.js
+++ 8-kyu/reversing-words-in-a-string.js
@@ -1,6 +1,15 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
 
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+// Example (Input --> Output)
+
+// "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+
+// Happy coding!
 
 
 
@@ -10,14 +19,15 @@
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: input will be a string of at least two words that may have 
+//R: return the string with the words reversed, words remained capitalized and if the word had a period after it the period moves with it
+//E: "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+//E: might have punctuation at the end of each string, might have straining spaces
 //P: Psuedo code -> see comments in solution
 
 
-
+//1 - 
 
 
 
