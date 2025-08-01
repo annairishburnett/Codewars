802d8d6350c2f887aa77e0c853d16c4545a8d6d3
Anna Burnett <annairishburnett@gmail.com>
2025-08-01 16:06:46 -0400

    added description and pseudo code

--- 8-kyu/dollars-and-cents.js
+++ 8-kyu/dollars-and-cents.js
@@ -26,9 +26,13 @@
 //P: see steps below
 
 //1 - create a function that takes in a number
-//2 - 
-//2 - use a template string with backticks `` to add the dollar sign
-//3 - use .padStart()
+//2 - convert to a string to to check if it includes a period using .toString and .includes
+//3 - if it doesn't have a period, then we know it's just dollars and no cents, so we can add a period and 00 at the end using a template literal
+//4 - if it does have a period, then we can check the length of the string to see if it's equal to three (3.3), if it is then we only add one zero using the template literal
+//  - return the value, call the function, pass in an argument, and console.log the result
+
+
+
 
 
 
