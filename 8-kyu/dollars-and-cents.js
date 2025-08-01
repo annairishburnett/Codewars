52661d1d513ec9b66fb0c4b4af1d0ae908c2c121
Anna Burnett <annairishburnett@gmail.com>
2025-08-01 16:06:10 -0400

    added description and pseudo code

--- 8-kyu/dollars-and-cents.js
+++ 8-kyu/dollars-and-cents.js
@@ -1,19 +1,34 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
 
+// 39.99 becomes $39.99
+
+// The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+// Examples:
+
+// 3 needs to become $3.00
+
+// 3.1 needs to become $3.10
+// Good luck! Your team knows they can count on you!
 
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
-//P:
+//P: input will be a number, an integer or floated value
+//R: return must be that number preceded by a dollar sign and a number with exactly two decimal places
+//E: 3 needs to become $3.00
+// 3.1 needs to become $3.10
+//E: add trailing zeros if they are missing, but no need to worry about any dangling periods
+//P: see steps below
 
-
+//1 - create a function that takes in a number
+//2 - 
+//2 - use a template string with backticks `` to add the dollar sign
+//3 - use .padStart()
 
 
 
