3063df73f2d985db9a40a2f2cb983519f48b0fd2
Anna Burnett <annairishburnett@gmail.com>
2025-09-16 09:42:30 -0400

    added description and PREP

--- 8-kyu/l1-bartender-drinks.js
+++ 8-kyu/l1-bartender-drinks.js
@@ -1,17 +1,42 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Complete the function that receives as input a string, and produces outputs according to the following table:
 
+// Input	Output
+// "Jabroni" ->	"Patron Tequila"
+// "School Counselor" -> "Anything with Alcohol"
+// "Programmer"	-> "Hipster Craft Beer"
+// "Bike Gang Member" -> "Moonshine"
+// "Politician"	-> "Your tax dollars"
+// "Rapper"	-> "Cristal"
+// anything else  -> "Beer"
+
+// Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+// Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
 
 
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
 
-//P: 
-//R: 
-//E: 
-//E:
+//P: In put will be a string. Ignore capitalization.
+//R: Return will be based on the table above, for example an input of "programmer" will result in an output of "hipster craft beer"
+//E: "Jabroni" ->	"Patron Tequila"
+// "School Counselor" -> "Anything with Alcohol"
+// "Programmer"	-> "Hipster Craft Beer"
+// "Bike Gang Member" -> "Moonshine"
+// "Politician"	-> "Your tax dollars"
+// "Rapper"	-> "Cristal"
+// anything else  -> "Beer"
+//E: if the input to the function is not any of the values in the table, then the return value should be "Beer". Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
 //P: see steps below
+
+//Pseudo Code
+//1 -> create a function that takes in a string
+//2 -> convert string to all lowercase using .toLowerCase()
+//3 -> use if/else statements and comparison === to check for each possibility in the table, jabroni - rapper and return appropriate output, the else should be "beer"
+//4 -> return the value, call the function, and console.log the result
 
 
 
