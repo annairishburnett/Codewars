a91f449744e39f9ad6aa4d0fe8fc6a061e968da7
Anna Burnett <annairishburnett@gmail.com>
2025-09-17 09:46:44 -0400

    added description and solution iterations

--- 8-kyu/string-templates-bug-fixing-5.js
+++ 8-kyu/string-templates-bug-fixing-5.js
@@ -1,8 +1,10 @@
 //CODEWARS CODING CHALLENGE DESCRIPTION:
 
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
 
-
-
+function buildString(...template){
+  return `I like #{template.join(',')}!`;
+}
 
 //MY SOLUTION:
 //Parameters, Return, Examples, Edge Cases, Pseudo Code:
@@ -16,7 +18,19 @@
 
 
 
+//1st SOLUTION ITERATION
+//Template strings have a $ instead of a # sign before the curly braces
 
+function buildString(...template){
+  return `I like ${template.join(',')}!`;
+}
+//Failed, spacing was wrong:
+    // Return the correct String: expected 'I like Cheese,Milk,Chocolate!' to equal 'I like Cheese, Milk, Chocolate!'
+
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
+//Success! 
 
 
 //TOP SOLUTIONS FROM CODEWARS USERS:
